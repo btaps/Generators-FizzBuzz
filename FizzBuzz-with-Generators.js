0e3789f1488118771bf983d68561e282db128f0d
Los Tapias <lostapias@Loss-MacBook-Pro.local>
2019-12-07 14:27:51 -0800

    Done

---
+++ FizzBuzz-with-Generators.js
@@ -0,0 +1,23 @@
+let numbers = 100
+
+let generateFizzBuzz = function* () {
+  for(i = 0; i<= numbers; i++){
+    if(i % 5 === 0 && i % 3 === 0) yield 'FizzBuzz'
+    else if (i % 5 === 0) console.log('Buzz')
+    else if (i % 3 === 0) console.log('Fizz')
+    else console.log(i)
+    }
+}
+let generatorObject = generateFizzBuzz()
+
+console.log(generatorObject.next())
+console.log(generatorObject.next())
+console.log(generatorObject.next())
+console.log(generatorObject.next())
+console.log(generatorObject.next())
+console.log(generatorObject.next())
+console.log(generatorObject.next())
+console.log(generatorObject.next())
+
+
+
